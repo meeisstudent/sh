ebc8b27199f2d7431def66e37145ca7e1e94fbb0
meeia.student <116544828+meeisstudent@users.noreply.github.com>
2022-12-03 12:46:03 +0800

    Update snell.sh

--- shell/snell.sh
+++ shell/snell.sh
@@ -129,7 +129,7 @@ echo "surge 配置文件"
 echo
 echo "=============================="
 echo
-echo "snell = snell,$(curl https://api.my-ip.io/ip -s),"$(cat /root/snell/snell-server.conf | grep "listen" | awk -F "=" '{print $2}' | sed 's/ //g')",psk=$(cat /root/snell/snell-server.conf | grep "psk" | awk -F "=" '{print $2}' | sed 's/ //g'),obfs=$(cat /root/snell/snell-server.conf | grep "obfs" | awk -F "=" '{print $2}' | sed 's/ //g'),version=4, reuse=true"
+echo "snell = snell,$(curl https://api.my-ip.io/ip -s),"$(cat /root/snell/snell-server.conf | grep "listen" | awk -F ":" '{print $2}' | sed 's/ //g')",psk=$(cat /root/snell/snell-server.conf | grep "psk" | awk -F "=" '{print $2}' | sed 's/ //g'),obfs=$(cat /root/snell/snell-server.conf | grep "obfs" | awk -F "=" '{print $2}' | sed 's/ //g'),version=4, reuse=true"
 echo
 echo "=============================="
 echo
